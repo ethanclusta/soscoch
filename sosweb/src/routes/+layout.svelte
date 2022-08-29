5ab82f26c60f00f3d9850f8dd206d30055996627
Ethan Corcoran <ethan@clusta.org>
2022-08-29 22:18:28 +0100

    attempt to flush extremely weird bug by commenting out layout.svelte

--- sosweb/src/routes/+layout.svelte
+++ sosweb/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<div class="relative flex flex-col my-10 mx-8 lg:mx-20">
+<!-- <div class="relative flex flex-col my-10 mx-8 lg:mx-20">
 	<div class="absolute right-0 top-0 text-white/80 hover:text-white cursor-pointer">Cymru</div>
 	<h1 class="text-red-400 font-sos text-7xl">Sôs Coch</h1>
 	<h1 class="text-white font-sos text-7xl">
@@ -11,4 +11,4 @@
 	<div class="mt-5 flex flex-col lg:flex-row gap-8">
 		<slot />
 	</div>
-</div>
+</div> -->
