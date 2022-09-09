69ab17a57853b2210e36f3cb41a34f4dde83e688
Ethan Corcoran <ethan@clusta.org>
2022-09-09 15:05:24 +0100

    update title in logo for tabernacl

--- sosweb/src/routes/+layout.svelte
+++ sosweb/src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	<div class="absolute right-0 top-0 text-white/80 hover:text-white cursor-pointer">Cymru</div>
 	<h1 class="text-red-400 font-sos text-5xl lg:text-7xl">Sôs Coch</h1>
 	<h1 class="text-white font-sos text-5xl lg:text-7xl">
-		Sinema <span class="text-xl lg:text-2xl text-green-500">at the Tabernacle</span>
+		Sinema <span class="text-xl lg:text-2xl text-green-500">at Y Tabernacl</span>
 	</h1>
 	<div class="mt-5 flex flex-col lg:flex-row gap-8">
 		<slot />
